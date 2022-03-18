894f0f94b30813770bb00fd48a5c977a13072839
Thibaud Fournès <thibaud.fournes@hotmail.fr>
2022-03-18 13:23:57 +0100

    Découvrir les props

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function Task() {
+function Task(props) {
   return (
     <li className="task">
       <div className="task__content">
-        <p className="task__label">Créer mon portfolio</p>
-		    <p className="task__priority task__priority-high">Elevée</p>
+        <p className="task__label">{props.libelle}</p>
+        <p className="task__priority task__priority-high">{props.priority}</p>
         <input type="text" className="task__input" placeholder="Modifier l'intitulé..." />
       </div>
       <button type="button" className="task__button">Fini !</button>
--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -4,10 +4,10 @@ import Task from './Task';
 function TasksList() {
   return (
     <ul>
-      <Task />
-	    <Task />
-	    <Task />
-	    <Task />
+      <Task libelle="Créer mon portfolio" priority="high" isDone={true}/>
+      <Task libelle="Publier un article sur Angular" priority="high" isDone={true} />
+      <Task libelle="Ecrire un article sur React et Redux" priority="med" isDone={false} />
+      <Task libelle="Créer un bloc personnalisé pour la page profil" priority="low" isDone={false} />
     </ul>
   );
 }
