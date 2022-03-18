bcce9b96b0acc4e871aa2ea38d65c356f6f3a55b
Thibaud Fournès <thibaud.fournes@hotmail.fr>
2022-03-18 13:26:23 +0100

    Utiliser les props pour changer le style de la priorité de chaque tâche

--- src/components/Task.js
+++ src/components/Task.js
@@ -5,7 +5,7 @@ function Task(props) {
     <li className="task">
       <div className="task__content">
         <p className="task__label">{props.libelle}</p>
-        <p className="task__priority task__priority-high">{props.priority}</p>
+        <p className={`task__priority task__priority-${props.priority}`}>{props.priority}</p>
         <input type="text" className="task__input" placeholder="Modifier l'intitulé..." />
       </div>
       <button type="button" className="task__button">Fini !</button>
