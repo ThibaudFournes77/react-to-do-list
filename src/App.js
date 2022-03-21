01c2b5fbeeabb71565b6f0fdbed3e9174d7afd26
Thibaud Fournès <thibaud.fournes@hotmail.fr>
2022-03-21 22:03:15 +0100

    Découvrir le hook useEffect - charger la liste des tâches depuis une API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Form from './components/Form';
 import TasksList from './components/TasksList';
-import { tasksList } from './data';
+
+async function getTasks() {
+  const tasks = await axios.get('http://localhost:5000');
+  return tasks.data;
+}
 
 function App() {
-  const tasks = tasksList;
+  const [tasks, setTasks] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(async () => {
+    const tasksFromApi = await getTasks();
+    setTasks(tasksFromApi);
+	  setLoading(false);
+  }, []);
+  
+
   return (
     <div>
-      <Form />
-      <TasksList tasks={tasks} />
+      {loading ?
+        (
+          <p>Patience...</p>
+        ) : (
+          <>
+            <Form />
+            <TasksList tasks={tasks} />
+          </>
+        )
+      }
     </div>
   );
 }
