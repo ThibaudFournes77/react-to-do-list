da4ee67ee8c3ffeb6ddeb0759247f1f26b550e32
Thibaud Fournès <thibaud.fournes@hotmail.fr>
2022-03-22 21:13:10 +0100

    Ajout d'une nouvelle tâche - gestion de la zone de texte

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Form() {
+  const [taskLibelle, setTaskLibelle] = useState('');
+
+  const handleTaskLibelleChange = (e) => {
+    setTaskLibelle(e.target.value);
+  }
+
   return (
     <form className="form">
 	    <h2 className="form__title">Ajouter une tâche</h2>
 	    <div className="form__fields">
 		  <div className="form__field">
 		    <label htmlFor="task-libelle" className="form__label">Intitulé</label>
-		    <input type="text" id="task-libelle" className="form__input" />
+		    <input type="text" id="task-libelle" className="form__input" value={taskLibelle} onChange={handleTaskLibelleChange} />
 		  </div>
 		  <div className="form__field">
 		    <label htmlFor="task-priority" className="form__label">Priorité</label>
